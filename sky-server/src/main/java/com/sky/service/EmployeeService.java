b0b902b70963fb01cf5279ef24e26538763a52a6
ywh <1450476215@qq.com>
2023-09-11 22:10:23 +0800

    分页查询、员工状态启用禁用

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -2,7 +2,9 @@ package com.sky.service;
 
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.result.PageResult;
 
 public interface EmployeeService {
 
@@ -15,9 +17,21 @@ public interface EmployeeService {
 
     /**
      * 新增员工
-     * @param employeeLoginDTO
+     * @param employeeDTO
      */
     public void save(EmployeeDTO employeeDTO);
 
+    /**
+     * 员工分页查询
+      * @param employeePageQueryDTO
+     * @return
+     */
+    PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 
+    /**
+     * 员工状态启用禁用
+     * @param status
+     * @param id
+     */
+    void startOrStop(Integer status, Long id);
 }
